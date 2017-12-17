c591f6a4c0a8ed5bdb9700ddf075882bebfd66e2
Muhammed Tanrikulu <md.tanrikulu@gmail.com>
2017-12-17 01:25:09 +0100

    Register and SignIn api implemented

--- app/components/SignUp.js
+++ app/components/SignUp.js
@@ -3,24 +3,30 @@ import { View } from "react-native";
 import { Card, Button, FormLabel, FormInput } from "react-native-elements";
 
 import { connect } from 'react-redux'
-import { onSignIn } from '../actions'
+import { onSignUp } from '../api/auth'
 
-const SignUpScreen = ({navigation, onSignIn}) => (
+const SignUpScreen = ({navigation}) => {
+  let nickname, password, repassword
+  return (
   <View style={{ paddingVertical: 20 }}>
     <Card>
-      <FormLabel>Email</FormLabel>
-      <FormInput placeholder="Email address..." />
+      <FormLabel>Nickname</FormLabel>
+      <FormInput ref={ el => nickname = el } placeholder="Email address..." />
       <FormLabel>Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Password..." />
-      <FormLabel>Confirm Password</FormLabel>
-      <FormInput secureTextEntry placeholder="Confirm Password..." />
+      <FormInput ref={ el => password = el } secureTextEntry placeholder="Confirm Password..." />
+      <FormLabel>Re-enter Password</FormLabel>
+      <FormInput ref={ el => repassword = el } secureTextEntry placeholder="Confirm Password..." />
 
       <Button
         buttonStyle={{ marginTop: 20 }}
         backgroundColor="#03A9F4"
         title="SIGN UP"
         onPress={() => {
-          onSignIn().then(() => navigation.navigate("SignedIn"));
+          onSignUp({
+            nickname: nickname.value,
+            password: password.value,
+            repassword: repassword.value
+          }).then(() => navigation.navigate("SignedIn"));
         }}
       />
       <Button
@@ -32,7 +38,7 @@ const SignUpScreen = ({navigation, onSignIn}) => (
       />
     </Card>
   </View>
-);
+)};
 
 function mapStateToProps (state) {
   return {
